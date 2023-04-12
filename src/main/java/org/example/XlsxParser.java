22348044000c19b28d504b92962ce2d4c0927ec3
WriteWrote <obukhova.elina@yandex.ru>
2023-04-12 12:41:02 +0300

    filtering cells p1

--- src/main/java/org/example/XlsxParser.java
+++ src/main/java/org/example/XlsxParser.java
@@ -5,7 +5,6 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.CellRangeAddress;
-import org.apache.poi.xssf.streaming.SXSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.example.model.CompletedSlot;
 
@@ -62,7 +61,7 @@ public class XlsxParser {
         return data;
     }
 
-    private static Map<String, Integer> countStartIndexRange(Row row) {
+    private static Map<String, Integer> countRowIndexRange(Row row) {
         Map<String, Integer> startIndexMap = new HashMap<>();
 
         String prevValue = row.getCell(0).getStringCellValue();
@@ -89,7 +88,7 @@ public class XlsxParser {
         for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {
             int finalI = i;
             var line = sortedByRow.stream()
-                    .filter(el -> el.getFirstRow()==finalI)
+                    .filter(el -> el.getFirstRow() == finalI)
                     .sorted(Comparator.comparing(CellRangeAddress::getFirstColumn))
                     .collect(Collectors.toList());
             sortedByCell.add(line);
@@ -97,17 +96,57 @@ public class XlsxParser {
         return sortedByCell;
     }
 
+    private static Map<String, Integer> countCellIndexRange(Sheet sheet, Integer column) {
+        //todo: finish countCellIndexRange()
+        return null;
+    }
+
+    private static List<List<CellRangeAddress>> includedInMergedRegion(Cell slot, List<List<CellRangeAddress>> regions) {
+        int cell = slot.getColumnIndex();
+        int row = slot.getRowIndex();
+
+        return regions.stream().
+                filter(el -> el.get(0).getFirstRow() <= row && el.get(0).getLastRow() >= row)
+                .collect(Collectors.toList());
+    }
+
     private static List<CompletedSlot> getCompletedSlots(Sheet sheet) {
-        Map<String, Integer> coursesIndexRange = countStartIndexRange(sheet.getRow(0));
-        Map<String, Integer> groupsIndexRange = countStartIndexRange(sheet.getRow(1));
+        // cells 1 - weekdays
+        // cells 2 - leson times
+        // row 1 - courses
+        // row 2 - groups
+        // row 3-4 - направления и специальности excluded for now
+        Map<String, Integer> coursesIndexRange = countRowIndexRange(sheet.getRow(0));
+        Map<String, Integer> groupsIndexRange = countRowIndexRange(sheet.getRow(1));
+        Map<String, Integer> weekdaysIndexRange = countCellIndexRange(sheet, 1);
+        Map<String, Integer> timesIndexRange = countCellIndexRange(sheet, 2);
 
         var mergedRegions = processMergedRegions(sheet);
 
-        var bannedRows = mergedRegions.stream().sorted()
-                .filter(el -> el.size()==1)
-                .collect(Collectors.toList());
+        for (int i = 5; i < sheet.getPhysicalNumberOfRows(); i++) {
+            int count = sheet.getRow(i).getLastCellNum();
+            for (int j = 3; j < count; j++) {
+                Cell currentCell = sheet.getRow(i).getCell(j);
+                if (currentCell.getStringCellValue().equals("")){
+                    var addressesInvolved = includedInMergedRegion(currentCell, mergedRegions);
+                    if (addressesInvolved.size()!=0){
 
+                    }
+                }
 
+                /* Algorithm:
+                 * get cell
+                 * check if it's empty -> check if it's in merged region
+                 * if in merged region: check if its first, then take info from it
+                 *                      form a Completed Slot
+                 *                      duplicate it for merged region
+                 * if no merged region: take info, form a Completed Slot
+                 *
+                 * to form a completed slot need to create empty slot, subject and teacher (in real project will be searching those in db
+                 * */
+
+            }
+        }
 
 
         return null;
