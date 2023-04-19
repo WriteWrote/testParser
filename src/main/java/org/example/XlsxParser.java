5f6155575ba108f598ad861e0cfa8b9414c4a185
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2023-04-19 22:43:50 +0300

    parsing one complete slot is done

--- src/main/java/org/example/XlsxParser.java
+++ src/main/java/org/example/XlsxParser.java
@@ -62,20 +62,28 @@ public class XlsxParser {
         return data;
     }
 
-    private static Map<String, Integer[]> countRowIndexRange(Row row, Integer offset) {
-        Map<String, Integer[]> indexMap = new HashMap<>();
+    private static Map<String, List<Integer[]>> countRowIndexRange(Row row) {
+        Map<String, List<Integer[]>> indexMap = new HashMap<>();
 
-        String prevValue = row.getCell(offset).getStringCellValue();
-        int prevIndex = offset;
-//        startIndexMap.put(prevValue, new Integer[]{0, 0});
+        String prevValue = row.getCell(0).getStringCellValue();
+        int prevIndex = 0;
 
         for (Cell cell : row) {
             String currentValue = cell.getStringCellValue();
+            List<Integer[]> buff;
 
             if (!prevValue.equals(currentValue) && !currentValue.equals("")) {
-                indexMap.put(prevValue, new Integer[]{prevIndex, cell.getColumnIndex() - 1});
-//                startIndexMap.put(currentValue, cell.getColumnIndex());
+                if (indexMap.containsKey(prevValue)) {
+                    buff = indexMap.get(prevValue);
+                    buff.add(new Integer[]{prevIndex, cell.getColumnIndex() - 1});
+                    indexMap.put(prevValue, buff);
+                } else {
+                    buff = new ArrayList<>();
+                }
 
+                buff.add(new Integer[]{prevIndex, cell.getColumnIndex() - 1});
+                indexMap.put(prevValue, buff)
+                ;
                 prevValue = currentValue;
                 prevIndex = cell.getColumnIndex();
             }
@@ -144,8 +152,8 @@ public class XlsxParser {
         // row 1 - courses
         // row 2 - groups
         // row 3-4 - направления и специальности excluded for now
-        Map<String, Integer[]> coursesIndexRange = countRowIndexRange(sheet.getRow(0), 0);     // все курсы занимают несколько ячеек
-        Map<String, Integer[]> groupsIndexRange = countRowIndexRange(sheet.getRow(1), 0);  // есть группы, занимающие либо 1, либо 2 ячейки
+        Map<String, List<Integer[]>> coursesIndexRange = countRowIndexRange(sheet.getRow(0));     // все курсы занимают несколько ячеек
+        Map<String, List<Integer[]>> groupsIndexRange = countRowIndexRange(sheet.getRow(1));  // есть группы, занимающие либо 1, либо 2 ячейки
         Map<String, List<Integer[]>> weekdaysIndexRange = countCellIndexRange(sheet, 0, 4);
         Map<String, List<Integer[]>> timesIndexRange = countCellIndexRange(sheet, 1, 4);
 
@@ -160,97 +168,110 @@ public class XlsxParser {
 
 //                if (addressesInvolved.size() != 0) {
 
-                    // todo check if it's first
+                // todo check if it's first
 
 
 //                } else {
-                    if (!currentCell.getStringCellValue().equals("")) {
-                        boolean denominator = currentCell.getRowIndex() % 2 != 0;
+                if (!currentCell.getStringCellValue().equals("")) {
+                    boolean denominator = currentCell.getRowIndex() % 2 != 0;
 
-                        int rowIndex = currentCell.getRowIndex();
-                        int columnIndex = currentCell.getColumnIndex();
+                    int rowIndex = currentCell.getRowIndex();
+                    int columnIndex = currentCell.getColumnIndex();
 
-                        String startTime = null;
-                        String endTime = null;
+                    String startTime = null;
+                    String endTime = null;
 
-                        boolean flag = false;
+                    boolean flag = false;
 
-                        for (var timesList : timesIndexRange.entrySet()) {
-                            for (var pair : timesList.getValue()) {
-                                if (rowIndex <= pair[1] && rowIndex >= pair[0]) {
-                                    startTime = timesList.getKey().split("-")[0].trim();
-                                    endTime = timesList.getKey().split("-")[1].trim();
-                                    flag = true;
-                                    break;
-                                }
-                            }
-                            if (flag){
-                                flag = false;
+                    for (var timesList : timesIndexRange.entrySet()) {
+                        for (var pair : timesList.getValue()) {
+                            if (rowIndex <= pair[1] && rowIndex >= pair[0]) {
+                                startTime = timesList.getKey().split("-")[0].trim();
+                                endTime = timesList.getKey().split("-")[1].trim();
+                                flag = true;
                                 break;
                             }
                         }
+                        if (flag) {
+                            flag = false;
+                            break;
+                        }
+                    }
 
-                        String weekdayName = "";
-                        for (var weekdaysList : weekdaysIndexRange.entrySet()) {
-                            for (var pair : weekdaysList.getValue()) {
-                                if (rowIndex <= pair[1] && rowIndex >= pair[0]) {
-                                    weekdayName = weekdaysList.getKey();
-                                    flag = true;
-                                    break;
-                                }
-                            }
-                            if (flag){
-                                flag = false;
+                    String weekdayName = "";
+                    for (var weekdaysList : weekdaysIndexRange.entrySet()) {
+                        for (var pair : weekdaysList.getValue()) {
+                            if (rowIndex <= pair[1] && rowIndex >= pair[0]) {
+                                weekdayName = weekdaysList.getKey();
+                                flag = true;
                                 break;
                             }
                         }
+                        if (flag) {
+                            flag = false;
+                            break;
+                        }
+                    }
 
-                        EmptySlot emptySlot = new EmptySlot(denominator, startTime, endTime, weekdayName);
+                    EmptySlot emptySlot = new EmptySlot(denominator, startTime, endTime, weekdayName);
 
-                        Integer course = null;
-                        Integer group = null;
-                        Integer subgroup = null;
+                    Integer course = null;
+                    Integer group = null;
+                    Integer subgroup = null;
 
-                        for (var entry : coursesIndexRange.entrySet()) {
-                            if (columnIndex <= entry.getValue()[1] &&
-                                    columnIndex >= entry.getValue()[0]) {
+                    for (var entry : coursesIndexRange.entrySet()) {
+                        for (var pair : entry.getValue()) {
+                            if (columnIndex <= pair[1] &&
+                                    columnIndex >= pair[0]) {
                                 course = Integer.parseInt(entry.getKey().split(" ")[0]);
+                                flag = true;
                                 break;
                             }
                         }
-                        for (var entry : groupsIndexRange.entrySet()) {
-                            if (columnIndex <= entry.getValue()[1] &&
-                                    columnIndex >= entry.getValue()[0]) {
+                        if (flag) {
+                            flag = false;
+                            break;
+                        }
+                    }
+                    for (var entry : groupsIndexRange.entrySet()) {
+                        for (var pair : entry.getValue()) {
+                            if (columnIndex <= pair[1] &&
+                                    columnIndex >= pair[0]) {
                                 group = Integer.parseInt(entry.getKey().split(" ")[0]);
                                 subgroup = columnIndex % 2 + 1;
+                                flag = true;
                                 break;
                             }
                         }
-                        // empty slot goes here
-                        CompletedSlot completedSlot = new CompletedSlot(0, 0, "", 0, 0, course, group, subgroup);
+                        if (flag) {
+                            break;
+                        }
                     }
+                    // empty slot goes here
+                    CompletedSlot completedSlot = new CompletedSlot(0, 0, "", 0, 0, course, group, subgroup);
                 }
-
-                /* Algorithm:
-                 * get cell
-                 * check if it's empty -> check if it's in merged region
-                 *      if in merged region: check if its first, then take info from it
-                 *                           form a Completed Slot
-                 *                           duplicate it for merged region
-                 *                           if not first, let it go, because first cell already was there before
-                 *      if no merged region:
-                 * if it's not empty: check if it's in merged region, then take info, form a Completed Slot, duplicate
-                 *
-                 * to form a completed slot need to create empty slot, subject and teacher (in real project will be searching those in db
-                 * */
-
-                // check if cell in merged region
-                //              yes: check if it's first
-                //                                yes: duplicate and form Completed Slots
-                //no: let it be
-                //              NO: if it's not empty, form completed slot
-
             }
+
+            /* Algorithm:
+             * get cell
+             * check if it's empty -> check if it's in merged region
+             *      if in merged region: check if its first, then take info from it
+             *                           form a Completed Slot
+             *                           duplicate it for merged region
+             *                           if not first, let it go, because first cell already was there before
+             *      if no merged region:
+             * if it's not empty: check if it's in merged region, then take info, form a Completed Slot, duplicate
+             *
+             * to form a completed slot need to create empty slot, subject and teacher (in real project will be searching those in db
+             * */
+
+            // check if cell in merged region
+            //              yes: check if it's first
+            //                                yes: duplicate and form Completed Slots
+            //no: let it be
+            //              NO: if it's not empty, form completed slot
+
+        }
 //        }
 
 
